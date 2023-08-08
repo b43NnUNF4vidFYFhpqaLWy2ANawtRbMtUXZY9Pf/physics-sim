bddaa8e7d9216ebd7b2ee6943a42ed251637d2af
b43NnUNF4vidFYFhpqaLWy2ANawtRbMtUXZY9Pf <77827828+b43NnUNF4vidFYFhpqaLWy2ANawtRbMtUXZY9Pf@users.noreply.github.com>
2023-08-08 21:00:26 +0200

    Refactor

--- src/physics/collision/resolution/constraints/plane_constraint.cpp
+++ src/physics/collision/resolution/constraints/plane_constraint.cpp
@@ -16,9 +16,10 @@ void PlaneConstraint::solve()
 {
     Vector2 v = rigid_body->get_vel();
     Vector2 omega = {0, 0, rigid_body->get_angVel()};
+    Vector2 C_t = v + omega.cross(r);
 
     float b = -beta*d;
-    float lambda = -m_eff*( (v + omega.cross(r)).dot(n) + b);
+    float lambda = -m_eff*(C_t.dot(n) + b);
     
     rigid_body->add_vel(inv_mass*n*lambda);
     rigid_body->add_angVel( (inv_inertia*tau_norm*lambda).z );
