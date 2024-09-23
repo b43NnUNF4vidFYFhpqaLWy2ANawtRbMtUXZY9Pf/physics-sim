c7ff11a21548a93d5a1cf2129673b93dd0eeb118
b43NnUNF4vidFYFhpqaLWy2ANawtRbMtUXZY9Pf <77827828+b43NnUNF4vidFYFhpqaLWy2ANawtRbMtUXZY9Pf@users.noreply.github.com>
2024-09-23 23:50:24 +0200

    feat(demo): use AABBTree, change constraint solver parameters and remove squares

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,7 +1,6 @@
 #include "window.h"
 #include "vector.h"
 #include "collision_body.h"
-#include "rigid_body.h"
 #include "bruteforce.h"
 #include "bvh.h"
 #include "world.h"
@@ -9,8 +8,6 @@
 #include "plane_solver.h"
 
 #include <vector>
-#include <memory>
-#include <utility>
 
 constexpr int SCREEN_WIDTH = 1280;
 constexpr int SCREEN_HEIGHT = 720;
@@ -20,63 +17,20 @@ int main()
     Physics::World world;
     Engine::Window window(SCREEN_WIDTH, SCREEN_HEIGHT, world);
 
-    Physics::Bruteforce collision_detector;
-    world.set_collision_detector(&collision_detector);
-
-    // Physics::AABBTree collision_detector(0.2);
+    // Physics::Bruteforce collision_detector;
     // world.set_collision_detector(&collision_detector);
+
+    Physics::AABBTree collision_detector(0.2);
+    world.set_collision_detector(&collision_detector);
 
     Physics::Vector2 gravity(0.0, -10*9.82);
     world.set_gravity(gravity);
 
-    Physics::PlaneConstraintSolver plane_solver(16, 0.8);
+    Physics::PlaneConstraintSolver plane_solver(6, 0.8);
     world.add_solver(&plane_solver);
-    Physics::ContactConstraintSolver contact_solver(10, 2, 0.5, 0.2);
+    Physics::ContactConstraintSolver contact_solver(6, 0.005, 0.05, 0.002);
     world.add_solver(&contact_solver);
 
-    const int square_size = 10;
-    const int columns = 50;
-    const int rows = 5;
-    const int column_interval = SCREEN_WIDTH/columns;
-    const int row_interval = column_interval;
-    const int row_height = rows*row_interval;
-    
-    const float square_mass = 100;
-    const float square_restitution = 0.5;
-    const float square_friction = 0.1;
-
-    std::vector<std::unique_ptr<Physics::RigidBody>> squares; // Physics engine itself does not take care of memory management
-    squares.reserve(columns*rows);
-    world.reserve_objects(columns*rows);
-
-    for (int i = 0; i < SCREEN_WIDTH; i += column_interval) {
-        for (int j = 0; j < row_height; j += row_interval) {
-            std::vector<Physics::Vector2> poly = {
-                    {
-                        static_cast<float>(i),
-                        static_cast<float>(SCREEN_HEIGHT - j - square_size)
-                    },
-                    {
-                        static_cast<float>(i),
-                        static_cast<float>(SCREEN_HEIGHT - j)
-                    },
-                    {
-                        static_cast<float>(i + square_size),
-                        static_cast<float>(SCREEN_HEIGHT - j)
-                    },
-                    {
-                        static_cast<float>(i + square_size),
-                        static_cast<float>(SCREEN_HEIGHT - j - square_size)
-                    }
-                };
-            std::unique_ptr<Physics::RigidBody> body = std::make_unique<Physics::RigidBody>(Physics::RigidBody(poly, square_mass, square_restitution, square_friction));
-            window.set_render_collisions(body.get());
-            world.add_object(body.get());
-            
-            squares.push_back(std::move(body));
-        }
-    }
-    
     std::vector<Physics::Vector2> left = {
         {50, 360},
         {50, 385},
